917e02c0421f77c5918e8e765afa89aae6a92561
Noémie Letenneur <n.letenneur@gmail.com>
2024-06-06 15:52:36 +0200

    TP07 - Fonctionnel mais pas optimisé

--- src/main/java/entites/Info100g.java
+++ src/main/java/entites/Info100g.java
@@ -1,5 +1,8 @@
 package entites;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class Info100g {
 	protected double energie;
 	protected double graisse;
@@ -23,4 +26,278 @@ public class Info100g {
 	protected double iron;
 	protected double fer;
 	protected double betaCarotene;
+	/** Constructor
+	 * It's empty by design since all the attributes are optional
+	 */
+	public Info100g() {
+	}
+	/** Getter pour energie
+	 * @return energie
+	 */
+	public double getEnergie() {
+		return energie;
+	}
+	/**Setter pour energie
+	 * @param energie energie 
+	 */
+	public void setEnergie(double energie) {
+		this.energie = energie;
+	}
+	/** Getter pour graisse
+	 * @return graisse
+	 */
+	public double getGraisse() {
+		return graisse;
+	}
+	/**Setter pour graisse
+	 * @param graisse graisse 
+	 */
+	public void setGraisse(double graisse) {
+		this.graisse = graisse;
+	}
+	/** Getter pour sucres
+	 * @return sucres
+	 */
+	public double getSucres() {
+		return sucres;
+	}
+	/**Setter pour sucres
+	 * @param sucres sucres 
+	 */
+	public void setSucres(double sucres) {
+		this.sucres = sucres;
+	}
+	/** Getter pour fibres
+	 * @return fibres
+	 */
+	public double getFibres() {
+		return fibres;
+	}
+	/**Setter pour fibres
+	 * @param fibres fibres 
+	 */
+	public void setFibres(double fibres) {
+		this.fibres = fibres;
+	}
+	/** Getter pour proteines
+	 * @return proteines
+	 */
+	public double getProteines() {
+		return proteines;
+	}
+	/**Setter pour proteines
+	 * @param proteines proteines 
+	 */
+	public void setProteines(double proteines) {
+		this.proteines = proteines;
+	}
+	/** Getter pour sel
+	 * @return sel
+	 */
+	public double getSel() {
+		return sel;
+	}
+	/**Setter pour sel
+	 * @param sel sel 
+	 */
+	public void setSel(double sel) {
+		this.sel = sel;
+	}
+	/** Getter pour vitA
+	 * @return vitA
+	 */
+	public double getVitA() {
+		return vitA;
+	}
+	/**Setter pour vitA
+	 * @param vitA vitA 
+	 */
+	public void setVitA(double vitA) {
+		this.vitA = vitA;
+	}
+	/** Getter pour vitD
+	 * @return vitD
+	 */
+	public double getVitD() {
+		return vitD;
+	}
+	/**Setter pour vitD
+	 * @param vitD vitD 
+	 */
+	public void setVitD(double vitD) {
+		this.vitD = vitD;
+	}
+	/** Getter pour vitE
+	 * @return vitE
+	 */
+	public double getVitE() {
+		return vitE;
+	}
+	/**Setter pour vitE
+	 * @param vitE vitE 
+	 */
+	public void setVitE(double vitE) {
+		this.vitE = vitE;
+	}
+	/** Getter pour vitK
+	 * @return vitK
+	 */
+	public double getVitK() {
+		return vitK;
+	}
+	/**Setter pour vitK
+	 * @param vitK vitK 
+	 */
+	public void setVitK(double vitK) {
+		this.vitK = vitK;
+	}
+	/** Getter pour vitC
+	 * @return vitC
+	 */
+	public double getVitC() {
+		return vitC;
+	}
+	/**Setter pour vitC
+	 * @param vitC vitC 
+	 */
+	public void setVitC(double vitC) {
+		this.vitC = vitC;
+	}
+	/** Getter pour vitB1
+	 * @return vitB1
+	 */
+	public double getVitB1() {
+		return vitB1;
+	}
+	/**Setter pour vitB1
+	 * @param vitB1 vitB1 
+	 */
+	public void setVitB1(double vitB1) {
+		this.vitB1 = vitB1;
+	}
+	/** Getter pour vitB2
+	 * @return vitB2
+	 */
+	public double getVitB2() {
+		return vitB2;
+	}
+	/**Setter pour vitB2
+	 * @param vitB2 vitB2 
+	 */
+	public void setVitB2(double vitB2) {
+		this.vitB2 = vitB2;
+	}
+	/** Getter pour vitPP
+	 * @return vitPP
+	 */
+	public double getVitPP() {
+		return vitPP;
+	}
+	/**Setter pour vitPP
+	 * @param vitPP vitPP 
+	 */
+	public void setVitPP(double vitPP) {
+		this.vitPP = vitPP;
+	}
+	/** Getter pour vitB6
+	 * @return vitB6
+	 */
+	public double getVitB6() {
+		return vitB6;
+	}
+	/**Setter pour vitB6
+	 * @param vitB6 vitB6 
+	 */
+	public void setVitB6(double vitB6) {
+		this.vitB6 = vitB6;
+	}
+	/** Getter pour vitB9
+	 * @return vitB9
+	 */
+	public double getVitB9() {
+		return vitB9;
+	}
+	/**Setter pour vitB9
+	 * @param vitB9 vitB9 
+	 */
+	public void setVitB9(double vitB9) {
+		this.vitB9 = vitB9;
+	}
+	/** Getter pour vitB12
+	 * @return vitB12
+	 */
+	public double getVitB12() {
+		return vitB12;
+	}
+	/**Setter pour vitB12
+	 * @param vitB12 vitB12 
+	 */
+	public void setVitB12(double vitB12) {
+		this.vitB12 = vitB12;
+	}
+	/** Getter pour calcium
+	 * @return calcium
+	 */
+	public double getCalcium() {
+		return calcium;
+	}
+	/**Setter pour calcium
+	 * @param calcium calcium 
+	 */
+	public void setCalcium(double calcium) {
+		this.calcium = calcium;
+	}
+	/** Getter pour magnesium
+	 * @return magnesium
+	 */
+	public double getMagnesium() {
+		return magnesium;
+	}
+	/**Setter pour magnesium
+	 * @param magnesium magnesium 
+	 */
+	public void setMagnesium(double magnesium) {
+		this.magnesium = magnesium;
+	}
+	/** Getter pour iron
+	 * @return iron
+	 */
+	public double getIron() {
+		return iron;
+	}
+	/**Setter pour iron
+	 * @param iron iron 
+	 */
+	public void setIron(double iron) {
+		this.iron = iron;
+	}
+	/** Getter pour fer
+	 * @return fer
+	 */
+	public double getFer() {
+		return fer;
+	}
+	/**Setter pour fer
+	 * @param fer fer 
+	 */
+	public void setFer(double fer) {
+		this.fer = fer;
+	}
+	/** Getter pour betaCarotene
+	 * @return betaCarotene
+	 */
+	public double getBetaCarotene() {
+		return betaCarotene;
+	}
+	/**Setter pour betaCarotene
+	 * @param betaCarotene betaCarotene 
+	 */
+	public void setBetaCarotene(double betaCarotene) {
+		this.betaCarotene = betaCarotene;
+	}
+	
+	
+
+
+
 }
--- src/main/java/entites/Produit.java
+++ src/main/java/entites/Produit.java
@@ -3,52 +3,229 @@ package entites;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "produit")
 public class Produit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected int id;
 	protected String libelle;
 	protected boolean presenceHuilePalme;
 	@ManyToOne
-	@JoinColumn(name="ID_Categorie")
+	@JoinColumn(name = "ID_Categorie")
 	private Categorie categorie;
 	@ManyToOne
-	@JoinColumn(name="ID_Marque")
+	@JoinColumn(name = "ID_Marque")
 	private Marque marque;
 	protected String scoreNutritionnel;
 	@ManyToMany
-	@JoinTable(name="produit_ingredient", joinColumns = { @JoinColumn(name = "id_ingredient") }, 
-    inverseJoinColumns = { @JoinColumn(name = "id_produit") } )
+	@JoinTable(name = "produit_ingredient", joinColumns = {
+			@JoinColumn(name = "id_ingredient") }, inverseJoinColumns = { @JoinColumn(name = "id_produit") })
 	private Set<Ingredient> ingredients = new HashSet<>();
 	@ManyToMany
-	@JoinTable(name="produit_additifs", joinColumns = { @JoinColumn(name = "id_additif") }, 
-    inverseJoinColumns = { @JoinColumn(name = "id_produit") } )
+	@JoinTable(name = "produit_additifs", joinColumns = { @JoinColumn(name = "id_additif") }, inverseJoinColumns = {
+			@JoinColumn(name = "id_produit") })
 	private Set<Additif> additifs = new HashSet<>();
 	@ManyToMany
-	@JoinTable(name="produit_allergene", joinColumns = { @JoinColumn(name = "id_allergene") }, 
-    inverseJoinColumns = { @JoinColumn(name = "id_produit") } )
+	@JoinTable(name = "produit_allergene", joinColumns = { @JoinColumn(name = "id_allergene") }, inverseJoinColumns = {
+			@JoinColumn(name = "id_produit") })
 	private Set<Allergene> allergenes = new HashSet<>();
+	@Embedded
+	protected Info100g info100g;
 
-	/** Constructor
+	/**
+	 * Constructor
+	 * 
 	 * @param libelle
 	 */
 	public Produit(String libelle) {
 		this.libelle = libelle;
 	}
 
-	/** Constructor jpa
+	/**
+	 * Constructor jpa
 	 * 
 	 */
 	public Produit() {
 	}
-	
+
+	@Override
+	public String toString() {
+		String produit = "id=" + id + ", libelle=" + libelle + ", presenceHuilePalme=" + presenceHuilePalme;
+		if (categorie != null) {
+			produit += ", categorie=" + categorie;
+		}
+		if (marque != null) {
+			produit += ", marque=" + marque;
+		}
+		if (scoreNutritionnel != null) {
+			produit += ", scoreNutritionnel=" + scoreNutritionnel;
+		}
+		if (ingredients != null) {
+			produit += ", ingredients=" + ingredients;
+		}
+		if (additifs != null) {
+			produit += ", additifs=" + additifs;
+		}
+		if (allergenes != null) {
+			produit += ", allergenes=" + allergenes;
+		}
+		if (info100g != null) {
+			produit += ", info100g=" + info100g;
+		}
+		return produit;
+	}
+
+	/** Getter pour id
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**Setter pour id
+	 * @param id id 
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/** Getter pour libelle
+	 * @return libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**Setter pour libelle
+	 * @param libelle libelle 
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/** Getter pour presenceHuilePalme
+	 * @return presenceHuilePalme
+	 */
+	public boolean isPresenceHuilePalme() {
+		return presenceHuilePalme;
+	}
+
+	/**Setter pour presenceHuilePalme
+	 * @param presenceHuilePalme presenceHuilePalme 
+	 */
+	public void setPresenceHuilePalme(boolean presenceHuilePalme) {
+		this.presenceHuilePalme = presenceHuilePalme;
+	}
+
+	/** Getter pour categorie
+	 * @return categorie
+	 */
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	/**Setter pour categorie
+	 * @param categorie categorie 
+	 */
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	/** Getter pour marque
+	 * @return marque
+	 */
+	public Marque getMarque() {
+		return marque;
+	}
+
+	/**Setter pour marque
+	 * @param marque marque 
+	 */
+	public void setMarque(Marque marque) {
+		this.marque = marque;
+	}
+
+	/** Getter pour scoreNutritionnel
+	 * @return scoreNutritionnel
+	 */
+	public String getScoreNutritionnel() {
+		return scoreNutritionnel;
+	}
+
+	/**Setter pour scoreNutritionnel
+	 * @param scoreNutritionnel scoreNutritionnel 
+	 */
+	public void setScoreNutritionnel(String scoreNutritionnel) {
+		this.scoreNutritionnel = scoreNutritionnel;
+	}
+
+	/** Getter pour ingredients
+	 * @return ingredients
+	 */
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	/**Setter pour ingredients
+	 * @param ingredients ingredients 
+	 */
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	/** Getter pour additifs
+	 * @return additifs
+	 */
+	public Set<Additif> getAdditifs() {
+		return additifs;
+	}
+
+	/**Setter pour additifs
+	 * @param additifs additifs 
+	 */
+	public void setAdditifs(Set<Additif> additifs) {
+		this.additifs = additifs;
+	}
+
+	/** Getter pour allergenes
+	 * @return allergenes
+	 */
+	public Set<Allergene> getAllergenes() {
+		return allergenes;
+	}
+
+	/**Setter pour allergenes
+	 * @param allergenes allergenes 
+	 */
+	public void setAllergenes(Set<Allergene> allergenes) {
+		this.allergenes = allergenes;
+	}
+
+	/** Getter pour info100g
+	 * @return info100g
+	 */
+	public Info100g getInfo100g() {
+		return info100g;
+	}
+
+	/**Setter pour info100g
+	 * @param info100g info100g 
+	 */
+	public void setInfo100g(Info100g info100g) {
+		this.info100g = info100g;
+	}
 	
 	
 
